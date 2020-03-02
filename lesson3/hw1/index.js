0cc3d9947606bd77b0e8eac177863f2897b58a01
unknown <gilyjmoon@gmail.com>
2020-03-02 19:58:17 +0200

    5

--- lesson3/hw1/index.js
+++ lesson3/hw1/index.js
@@ -10,7 +10,7 @@ const isLoggedIn = Boolean(userName);
 /* В коде ниже нужно использовать правильные операторы после знака присваивания */
 const activeUser = anotherUser || defaultUserName;
 const hasAccess = isAdmin || isLoggedIn;
-const isTruthy = message === anotherMessage;
+const isTruthy = message < anotherMessage;
 const isFalsy = !isLoggedIn;
 const isNotTrue = typeof message === 'number';
 const isTrue = typeof message == 'boolean';
