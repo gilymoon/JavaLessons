b422e9a628d1e27208a6ca27aae6dd3c65e15620
unknown <gilyjmoon@gmail.com>
2020-03-02 17:51:08 +0200

    1

--- lesson3/task1/index.js
+++ lesson3/task1/index.js
@@ -1,8 +1,8 @@
 // Унарный плюс приводит операнд к числу
 +17;
-console.log('+17', +17);
+console.log('+17: ', 17);
 +'77';
-console.log('\'+17\': ', +'77');
+console.log('\'+77\': ', +'77');
 // Бинарный плюс складывает строки или прибавляет числа
 10 + '5';
 console.log('10 + \'5\': ', 10+'5');
@@ -15,9 +15,9 @@ null + 8;
 console.log('null + 8', null + 8);
 undefined + '5';
 7 - null;
-console.log('7 - null', 7 - null);
+console.log('7 - null: ', 7 - null);
 5 - undefined;
-console.log('5 - undefined', 5-undefined);
+console.log('5 - undefined: ', 5-undefined);
 'text' + null;
 console.log('\'text\' + null', 'text'+null);
 let num = 17;
