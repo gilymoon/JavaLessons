30964b36b62fceb3a41d0531b148477a44a62c31
unknown <gilyjmoon@gmail.com>
2020-03-03 15:06:09 +0200

    1

---
+++ lesson4/hw3/index.js
@@ -0,0 +1,20 @@
+let n = 0;
+let m = 1000;
+let sum = 0;
+
+while(n <= m){
+    if(n%2 != 0){
+        console.log('Found');
+        sum += n;
+        if(sum * 5 > 5000){
+            console.log('Bigger');
+        }
+        else if(sum * 5 === 5000){
+            console.log('equal');
+        }
+        else{
+            console.log('Smaller');
+        }
+    }
+    n++;
+}
