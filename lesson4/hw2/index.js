ae55f9e4e021639c4584e7b7ff5cd556122b0c27
unknown <gilyjmoon@gmail.com>
2020-03-03 14:16:44 +0200

    1

---
+++ lesson4/hw2/index.js
@@ -0,0 +1,20 @@
+let m = 1;
+let n = 20;
+let i = m;
+
+let reuslt = 0;
+
+while(i < n){
+    if(i % 2 === 0 && i % 4 === 0){
+        reuslt += i;
+    }
+    else if(i % 3 === 0){
+        result -= i;
+    }
+    else if(i % 4 === 0){
+        result *= i;
+    }
+    else if(i % 5 === 0){
+        console.log(i);
+    }
+}
