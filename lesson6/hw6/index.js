c1d2048563e6204b0aaf3e10b126fd722b7c5011
unknown <gilyjmoon@gmail.com>
2020-03-04 20:13:18 +0200

    2

--- lesson6/hw6/index.js
+++ lesson6/hw6/index.js
@@ -1,10 +1,12 @@
-function withdraw(clients, balances, client, amount) {
-    for (let i = 0; i < clients.length; i++) {
-        if (clients[i] == client) {
-            if (balances[i] > amount) {
-                return balances[i] -= amount
-            } else {
-                return -1
+function withdraw(clients, balances, client, amount){
+    for(let i = 0; i < clients.length; i++){
+        if(clients[i]===client){
+            if(balances[i] > amount){
+
+                return balances[i] -= amount;
+            }
+            else{
+                return -1;
             }
         }
     }
