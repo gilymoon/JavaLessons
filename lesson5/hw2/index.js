66d6ad47a4e205972777256fd5d6c028b7bd8f3b
unknown <gilyjmoon@gmail.com>
2020-03-04 12:09:33 +0200

    2

--- lesson5/hw2/index.js
+++ lesson5/hw2/index.js
@@ -1,10 +1,18 @@
-function getPrimes(a){
-    for (let i = 2; i <= n; i++) {
-
-        for (let j = 2; j < i; j++) {
-            if (i % j === 0)  console.log(i);
-        }
-       
+function isPrime(n) {
+    let isPrime = true;
+    for (let j = 2; j < n; j++) {
+      // console.log('small j = ', j, ' i = ', i);
+      if (n % j === 0) {
+        isPrime = false;
+      }
     }
-}
-
+    return isPrime;
+  }
+  function primeNumbners(n) {
+      for (let i = 2; i <= n; i++) {
+          // console.log('big i = ', i);
+          if (isPrime(i)) {
+            console.log(i);
+          }
+      }
+  }
