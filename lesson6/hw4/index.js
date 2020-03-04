8a96e79baceefa6f03ce6604610a642c8fd792b0
unknown <gilyjmoon@gmail.com>
2020-03-04 17:17:51 +0200

    2

--- lesson6/hw4/index.js
+++ lesson6/hw4/index.js
@@ -1,9 +1,9 @@
 function uniqueCount(array){
     let count = 0;
-    if(array.length<0)break;
+    
     for(let i = 0; i < array.length; i++){
         for(let j = 0; j < array.length; j++){
-            if(array[j] === array[i]){
+            if(array[i] === array[j]){
                 count++;
             }
         }
