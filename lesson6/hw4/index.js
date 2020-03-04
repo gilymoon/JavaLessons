cb0c5844e33bd405446f75f8026b1d0ff5b1c58e
unknown <gilyjmoon@gmail.com>
2020-03-04 18:58:47 +0200

    6

--- lesson6/hw4/index.js
+++ lesson6/hw4/index.js
@@ -1,27 +1,22 @@
-function isIncluded(arr, num){
-    let isIncl = false;
-    for(let i = 0; i < arr.length; i++){
-        if(num === arr[i]){
-            isIncl = true;
-        }
-    }
-    if(isIncl){
-        return true;
-    }
-}
-
-function uniqueCount(array){
+function uniqueCount(array) {
+    let uniqArray = [];
     let count = 0;
-    
-    for(let i = 0; i < array.length; i++){
-        if(isIncluded(array, array[i])){
+    for (let i = 0; i < array.length; i++) {
+        let uniq = false;
+        for (let j = 0; j < uniqArray.length; j++) {
+            if (array[i] === uniqArray[j]) {
+                uniq = true;
+            }
+        }
+        if (uniq == false) {
+            uniqArray.push(array[i]);
             count++;
         }
-        
     }
+    console.log(uniqArray, count);
+
     return count;
 }
-
 let arr = [66, 2, 66, 3, 1];
 console.log(uniqueCount(arr));
 
