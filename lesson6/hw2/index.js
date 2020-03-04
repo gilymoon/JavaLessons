8d1efd66ad102b08247a32572e2fd5dcaa52dad2
unknown <gilyjmoon@gmail.com>
2020-03-04 16:08:06 +0200

    2

--- lesson6/hw2/index.js
+++ lesson6/hw2/index.js
@@ -1,11 +1,12 @@
 function reverseArray(arr){
     if(!Array.isArray(arr)) return null;
     let arr2 = arr;
-    let arr3 = [];
-    for(let i = 0; i < arr.length; i++){
-        arr3[i] = arr2.length-i;
+    let arr3 = [arr.length];
+    for(let i = arr.length; i > 0; i--){
+        arr3[i] = arr2[arr2.length - i];
     
     }
+    arr3.shift();
     return arr3;
 }
 
