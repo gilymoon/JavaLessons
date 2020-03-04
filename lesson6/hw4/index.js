aed94d3fd1ca33291991001166b5d975b778b6b8
unknown <gilyjmoon@gmail.com>
2020-03-04 17:16:24 +0200

    1

---
+++ lesson6/hw4/index.js
@@ -0,0 +1,11 @@
+function uniqueCount(array){
+    let count = 0;
+    for(let i = 0; i < array.length; i++){
+        for(let j = 0; j < array.length; j++){
+            if(array[j] === array[i]){
+                count++;
+            }
+        }
+    }
+    return count;
+}
