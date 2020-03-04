2bc00636dfa22a911be9913b75c880eb42916ee1
unknown <gilyjmoon@gmail.com>
2020-03-04 14:51:45 +0200

    1

--- lesson6/hw1/index.js
+++ lesson6/hw1/index.js
@@ -1,6 +1,6 @@
 function squareArray(arr){
     if(!Array.isArray(arr)) return null;
-    let arr2 = arr;
+    const arr2 = arr;
     for(let i = 0; i < arr.length; i++){
         arr2[i] = arr[i] * arr[i]; 
     }
