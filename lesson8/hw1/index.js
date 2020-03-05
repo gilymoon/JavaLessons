782f5266b5058d8b605aebd1c70d7c5b3787077b
unknown <gilyjmoon@gmail.com>
2020-03-05 15:12:50 +0200

    1

--- lesson8/hw1/index.js
+++ lesson8/hw1/index.js
@@ -1,10 +1,9 @@
 function pickProps(obj, arr){
     const newObj = {};
-    for(let key in obj){
-        
-        for(let i = 0; i < arr.length; i++){
-            if(obj[key] === arr[i]){
-                newObj[key]=obj[key];
+    for (let i of arr) {
+        for (let key in obj) {
+            if (i === key) {
+                newObj[key] = obj[key]
             }
         }
     }
