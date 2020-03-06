6997d54fdb846990db0fb4016e8fc1ad9478e070
unknown <gilyjmoon@gmail.com>
2020-03-06 17:55:50 +0200

    1

--- lesson10/hw2/index.js
+++ lesson10/hw2/index.js
@@ -1,5 +1,5 @@
 function getRandomNumbers(l, s, f){
     if (Math.floor(s) >= Math.floor(f)) return null;
-    const arr = [l];
+    let arr = [l];
     return arr.fill().map(num => Math.round(Math.random() * (s - f) + f))
 }
