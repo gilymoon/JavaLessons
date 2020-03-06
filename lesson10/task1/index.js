a7632ceaf4fc1ee218fc3e210bb42a0d16703cc8
unknown <gilyjmoon@gmail.com>
2020-03-06 16:49:12 +0200

    1

--- lesson10/task1/index.js
+++ lesson10/task1/index.js
@@ -1,14 +1,14 @@
 const getFiniteNumbers = arr =>
-    arr.forEach(num => Number.isFinite(num));
+    arr.filter(num => Number.isFinite(num));
 
 const getFiniteNumbersV2 = arr =>
     arr.filter(num => isFinite(num));
 
 const getNaN = arr =>
-    arr.map(num => Number.isNaN(num));
+    arr.filter(num => Number.isNaN(num));
 
 const getNaNV2 = arr =>
-    arr.forEach(num => isNaN(num));
+    arr.filter(num => isNaN(num));
 
 const getIntegers = arr => 
     arr.filter(num => Number.isInteger(num));
