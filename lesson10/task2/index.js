1bada5f9dc0ad6d564a8587a102e31b879b6b91b
unknown <gilyjmoon@gmail.com>
2020-03-06 16:53:53 +0200

    2

---
+++ lesson10/task2/index.js
@@ -0,0 +1,13 @@
+const getOarsedIntegers = arr =>
+    arr.map(num => Number.parseInt(num));
+
+const getOarsedIntegersV2 = arr => 
+    arr.map(num => parseInt(num));
+
+
+const getOarsedFloat = arr => 
+    arr.map(num => Number.parseFloat(num));
+
+const getOarsedFloatV2 = arr =>
+    arr.map(num => parseFloat(num));
+
