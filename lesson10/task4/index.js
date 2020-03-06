9d2f43928c9079aed112594ba83d67ec037a8e6a
unknown <gilyjmoon@gmail.com>
2020-03-06 17:22:13 +0200

    1

--- lesson10/task4/index.js
+++ lesson10/task4/index.js
@@ -1,6 +1,7 @@
 function getMaxAbsoluteNumber(arr){
-    const absoluteValues = arr.map(num => Math.abs(num));
     if(!Array.isArray(arr)||arr.length()===0) return null;
+    const absoluteValues = arr.map(num => Math.abs(num));
+   
     return Math.max(...absoluteValues);
     
 }
