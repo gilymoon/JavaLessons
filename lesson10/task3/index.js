00957fa7161cdeeec1eb89d4dd0a56a5d92620b9
unknown <gilyjmoon@gmail.com>
2020-03-06 17:00:39 +0200

    1

--- lesson10/task3/index.js
+++ lesson10/task3/index.js
@@ -1,6 +1,6 @@
 function multiRound(num){
     let arr = [Math.floor(num), 
     Math.round(num), Math.ceil(num), 
-Math.trunc(num), num.toFixed(2)];
+Math.trunc(num), +num.toFixed(2)];
 return arr;
 }
