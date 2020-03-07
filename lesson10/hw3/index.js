d6d13277ac3d4b7e4b7315f636c8eb7664381237
unknown <gilyjmoon@gmail.com>
2020-03-07 22:43:13 +0200

    2

--- lesson10/hw3/index.js
+++ lesson10/hw3/index.js
@@ -1,7 +1,9 @@
 function superRound(num, acc){
     let coma = Math.pow(10, acc);
     let arr = [Math.floor(num * coma)/coma, 
-        Math.round(num * coma)/coma, Math.ceil(num*coma)/coma, 
-    Math.trunc(num*coma)/coma, +num.toFixed(2)];
+        Math.round(num * coma) / coma, 
+        Math.ceil(num * coma) / coma, 
+    Math.trunc(num * coma) / coma, 
+    +num.toFixed(coma)];
     return arr;
 }
