775af1d7471bfcd844627a2c2a21b4bbe2b53d89
unknown <gilyjmoon@gmail.com>
2020-03-07 22:32:48 +0200

    1

--- lesson10/hw2/index.js
+++ lesson10/hw2/index.js
@@ -2,7 +2,7 @@ function getRandomNumbers(len, start, finish){
     if (Math.floor(start) >= Math.floor(finish)) return null;
     
     
-    return Array(len).fill().map(num => Math.round(Math.random() * (start - finish + 1) + finish))
+    return Array(len).fill().map(num => Math.floor(Math.random() * (start - finish + 1) + finish))
 }
 
 console.log(getRandomNumbers(10, -0.1, 0.1));
