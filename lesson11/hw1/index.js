14402b2f90b5cf42b4f1a7e1afe0601e53b93804
unknown <gilyjmoon@gmail.com>
2020-03-09 15:43:10 +0200

    3

--- lesson11/hw1/index.js
+++ lesson11/hw1/index.js
@@ -5,6 +5,10 @@ function splitString(str, len){
         while(true){
            let chunk = str.substr(startPosition, len);
            if(chunk.length === 0) break;
+           if (chunk.length < len) {
+            chunk = chunk + '.'.repeat(len - chunk.length);
+            
+        }
            if (len === undefined) {
                strArr.push(chunk.slice(0));
                startPosition += 10;
@@ -12,10 +16,12 @@ function splitString(str, len){
                strArr.push(chunk.slice(0));
                startPosition += len;
            }
-   
+           
         }
         return strArr;
     
 }
+let str = 'abcd efgh';
+console.log(splitString(str, 4));
 
 // 1. [abcd efgh] => ['abcd', ' efg', 'h...'];
