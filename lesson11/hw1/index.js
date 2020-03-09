84c5330b77d1ea84acfc3767ebd7c8b41f008456
unknown <gilyjmoon@gmail.com>
2020-03-09 16:01:26 +0200

    1

--- lesson11/hw1/index.js
+++ lesson11/hw1/index.js
@@ -1,27 +1,27 @@
 function splitString(str, len){
-     const strArr = [];
-        let startPosition = 0;
-       if (str === !(String)) return null;
-        while(true){
-           let chunk = str.substr(startPosition, len);
-           if(chunk.length === 0) break;
-           if (chunk.length < len) {
-            chunk = chunk + '.'.repeat(len - chunk.length);
-            
-        }
-           if (len === undefined) {
-               strArr.push(chunk.slice(0));
-               startPosition += 10;
-           } else {
-               strArr.push(chunk.slice(0));
-               startPosition += len;
-           }
+    if(len === undefined){
+        len = 10;
+    }
+    const strArr = [];
+       let startPosition = 0;
+      if (str === !(String)) return null;
+       while(true){
+          let chunk = str.substr(startPosition, len);
+          if(chunk.length === 0) break;
+          if (chunk.length < len) {
+           chunk = chunk + '.'.repeat(len - chunk.length);
            
-        }
-        return strArr;
-    
+       }
+           
+              strArr.push(chunk.slice(0));
+              startPosition += len;
+          
+          
+       }
+       return strArr;
+   
 }
-let str = 'abcd efgh';
-console.log(splitString(str, 4));
+let str = 'abcd efgh hagsd';
+console.log(splitString(str, ));
 
 // 1. [abcd efgh] => ['abcd', ' efg', 'h...'];
