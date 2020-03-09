6e287f6fc069dc38ff315e56c08085c79f707312
unknown <gilyjmoon@gmail.com>
2020-03-09 23:00:21 +0200

    1

--- lesson6/task7/index.js
+++ lesson6/task7/index.js
@@ -1,8 +1,10 @@
 function increaseEvenEl(arr, delta){
+    let arr1 = arr;
     if(!Array.isArray(arr)) return null;
     for(let i = 0; i < arr.length; i++){
-        if(arr[i]%2===0){
-            arr[i] += delta;
+        if(arr1[i]%2===0){
+            arr1[i] += delta;
         }
     }
+    return arr1;
 }
