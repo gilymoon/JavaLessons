6b7375760f71e13507dce75a8fba581b09f55dae
unknown <gilyjmoon@gmail.com>
2020-03-09 15:22:26 +0200

    1

---
+++ lesson11/hw1/index.js
@@ -0,0 +1,21 @@
+function splitString(str, num){
+     const strArr = [];
+        let startPosition = 0;
+       if (str === !(String)) return null;
+        while(true){
+           let chunk = str.substr(startPosition, len);
+           if(chunk.length === 0) break;
+           if (len === undefined) {
+               strArr.push((chunk[0].toUpperCase()) + chunk.slice(1));
+               startPosition += 10;
+           } else {
+               strArr.push((chunk[0].toUpperCase()) + chunk.slice(1));
+               startPosition += len;
+           }
+   
+        }
+        return strArr;
+    
+}
+
+// 1. [abcd efgh] => ['abcd', ' efg', 'h...'];
