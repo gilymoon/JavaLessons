e02dfeec32eb99170052ec13d46b01388ecfe270
unknown <gilyjmoon@gmail.com>
2020-03-09 11:53:36 +0200

    2

--- lesson11/task1/index.js
+++ lesson11/task1/index.js
@@ -1,13 +1,16 @@
  function splitText(str, len){
      const strArr = [];
      let startPosition = 0;
-
+    if (!str) return null;
      while(true){
         let chunk = text.substr(startPosition, len);
-        if(chunk.length === 0) break;
-        strArr.push(chunk[0].toUpperCase()+chunk.slice(1));
-        startPosition += len;
-
+        if (len === undefined) {
+            strArr.push((chunk[0].toUpperCase()) + chunk.slice(1));
+            startPosition += 10;
+        } else {
+            strArr.push((chunk[0].toUpperCase()) + chunk.slice(1));
+            startPosition += len;
+        }
 
      }
      return strArr.join('\n');
