a0b23328255b476d18e38e9e9c8af1815e440753
unknown <gilyjmoon@gmail.com>
2020-03-09 17:40:51 +0200

    4

--- lesson11/hw2/index.js
+++ lesson11/hw2/index.js
@@ -1,9 +1,13 @@
 function countOccurrences(str, searchStr){
     if(searchStr === '') return null;
-    return str.split(arg).length;
+    return str.split(searchStr).length-1;
     
     
      
 }
 
 
+let str = 'str strstr 3 times';
+let searchStr2 = 'str';
+console.log(str.length);
+console.log(countOccurrences(str, searchStr2));
