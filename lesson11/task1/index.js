e441bba43c1bf7284600777738a22896f0208495
unknown <gilyjmoon@gmail.com>
2020-03-09 11:48:51 +0200

    1

---
+++ lesson11/task1/index.js
@@ -0,0 +1,14 @@
+ function splitText(str, len){
+     const strArr = [];
+     let startPosition = 0;
+
+     while(true){
+        let chunk = text.substr(startPosition, len);
+        if(chunk.length === 0) break;
+        strArr.push(chunk[0].toUpperCase()+chunk.slice(1));
+        startPosition += len;
+
+
+     }
+     return strArr.join('\n');
+ }
