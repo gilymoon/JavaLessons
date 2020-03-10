33a19582dc24f9bb23240c897c861f8705477a88
unknown <gilyjmoon@gmail.com>
2020-03-10 12:59:59 +0200

    1

--- lesson7/task7/index.js
+++ lesson7/task7/index.js
@@ -1,7 +1,4 @@
 function withdraw(clients, balances, client, amount){
     let clientPos = clients.findIndex(client);
-    if(balances[clientPos] > amount){
-
-    }
     return balances[clientPos] > amount ? balances[clientPos] -= amount : -1; 
 }
