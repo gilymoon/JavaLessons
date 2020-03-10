7982e5e96b5b4cb747c456cd92b6a720f55b61a2
unknown <gilyjmoon@gmail.com>
2020-03-10 14:14:48 +0200

    2

--- lesson7/task9/index.js
+++ lesson7/task9/index.js
@@ -1,9 +1,13 @@
 function filterNames(arr, text){
    return arr.filter(el => {
-        if(el.length()> 5){
+
+        if(el.length > 5){
             if(el.includes(text)){
                 return el;
             }
         }
     })
 }
+
+let arr = ['John', 'Olivya', 'Oleksand', 'Vanya'];
+console.log(arr, 'ya');
