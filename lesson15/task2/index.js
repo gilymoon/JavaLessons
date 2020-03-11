af44991e03a9cf031882b58ccc3d35189fdcbac6
unknown <gilyjmoon@gmail.com>
2020-03-11 20:02:21 +0200

    1

---
+++ lesson15/task2/index.js
@@ -0,0 +1,25 @@
+export default function createLogger () {
+    let memo = 0;
+
+    const add = (num) => {
+        memo += num;
+    };
+
+    const decrease = (num2) => {
+        memo -= num2;
+    };
+
+    const reset = () => {
+        memo = 0;
+    };
+
+    const getMemo = () => {
+        return memo;
+    };
+    return {
+        add,
+        decrease,
+        reset,
+        getMemo,
+    }
+}
