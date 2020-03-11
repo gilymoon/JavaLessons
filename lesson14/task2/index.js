cb65ea62f3ee5f90cb43a3e132ac46112a1080b1
unknown <gilyjmoon@gmail.com>
2020-03-11 17:34:07 +0200

    1

---
+++ lesson14/task2/index.js
@@ -0,0 +1,22 @@
+function createMessenger() {
+    let message = 'Just learn it!';
+    let sender = 'Gromcode';
+
+    const sendMessage = (name) => {
+        console.log(`${name}, ${message} Your ${sender}`);
+    };
+    
+    const setMessage = (text) => {
+        message = text;
+    };
+
+    const setSender = (newSender) => {
+        sender = newSender;
+    };
+
+    return {
+        sendMessage,
+        setMessage,
+        setSender,
+    }
+}
