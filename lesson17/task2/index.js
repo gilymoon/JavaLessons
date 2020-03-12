0616f4e204f924454b361339a639977800a84e39
unknown <gilyjmoon@gmail.com>
2020-03-12 18:39:17 +0200

    1

---
+++ lesson17/task2/index.js
@@ -0,0 +1,10 @@
+const callbackPrompt = {
+    message: 'Tell me your number', 
+    showPrompt() {
+        const phoneNumber = prompt(this.message);
+        console.log(phoneNumber);
+    },
+    showDeferredPrompt(ms){
+        setTimeout(this.showPrompt(), 1000)
+    }
+}
