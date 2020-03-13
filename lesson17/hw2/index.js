ad3b1bea47f86b9bbf2671bff41f1d072201334d
unknown <gilyjmoon@gmail.com>
2020-03-13 12:47:03 +0200

    1

---
+++ lesson17/hw2/index.js
@@ -0,0 +1,28 @@
+const timer = {
+    secondsPassed: 0,
+    minsPassed: 0, 
+    startTimer(){
+        setTimeout(() => {
+            if(secondsPassed>59){
+                this.secondsPassed = 0;
+                this.minsPassed++;
+            } 
+            return this.secondsPassed+5;
+        }, 5000)
+        },
+    getTime(){
+        if(this.secondsPassed<10){
+            let secBefore10 = '0'+this.secondsPassed;
+        }
+        return `${this.minsPassed}:${secBefore10}`;
+    },
+    stopTimer() {
+        clearInterval(this.start)
+    },
+    resetTimer() {
+        this.secondsPassed = 0;
+        this.minsPassed = 0;
+    }
+}
+
+export{timer}
