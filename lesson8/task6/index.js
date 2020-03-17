439e0fbe6ab6c41f8896668d7749a2012932abe9
unknown <gilyjmoon@gmail.com>
2020-03-17 20:37:26 +0200

    1

---
+++ lesson8/task6/index.js
@@ -0,0 +1,31 @@
+const userData = {
+    name:'Sam'
+  };
+  
+  const userId = 'asd';
+  
+  function addPropertyV1 (userData, userId) {
+    userData.id = userId;
+    return userData;
+  }
+  
+  function addPropertyV2 (userData, userId) {
+    userData.id = userId
+    const newObj = Object.assign({}, userData);
+  
+    return newObj;
+  }
+  
+  const addPropertyV3 = (userData, userId) => {
+    const newObj = Object.assign({}, userData);
+    newObj.id = userId;
+  
+    return newObj;
+  };
+  
+  const addPropertyV4 = (userData, userId) => {
+    const newObj = {...userData };
+    newObj.id = userId;
+  
+    return newObj;
+  };
