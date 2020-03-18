dc22348171142988355e145ea628ba1955a9a15c
unknown <gilyjmoon@gmail.com>
2020-03-18 11:50:44 +0200

    2

---
+++ lesson8/task10/index.js
@@ -0,0 +1,10 @@
+function buildObject(keysList, valuesList) {
+    const arr = [];
+
+     for (let i = 0; i < keysList.length; i++) {
+        arr.push([keysList[i], valuesList[i]]);
+    }
+    const obj = Object.fromEntries(arr);
+
+    return obj;
+};
