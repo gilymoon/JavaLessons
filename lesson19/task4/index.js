710547340ed8a379282b60bf20b176862b684fbf
unknown <gilyjmoon@gmail.com>
2020-03-20 19:18:33 +0200

    1

--- lesson19/task4/index.js
+++ lesson19/task4/index.js
@@ -3,3 +3,19 @@ function User(name, age){
     this.age = age;
 }
 
+User.prototype.sayHi = function(){
+    console.log(`Hi, I am ${this.name}`)
+}
+
+User.prototype.requestNewPhoto = function(){
+    console.log(`New photo request was sent for ${name}`)
+}
+
+User.prototype.setAge = function(age){
+    this.age = age;
+    if(age >= 25){
+        console.log(`New photo request was sent for ${name}`)
+    }
+    if(age < 0) return false;
+    else return age;
+}
