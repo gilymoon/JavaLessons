2882c65974a9d08740fe847a093378f94d1dbbab
unknown <gilyjmoon@gmail.com>
2020-03-20 16:45:55 +0200

    1

---
+++ lesson19/task2/index.js
@@ -0,0 +1,11 @@
+function getOwnProps (obj) {
+    const arr = [];
+    for (let prop in obj) {
+        if (obj.hasOwnProperty(prop) && typeof obj[prop] !== 'function') {
+            arr.push(prop)
+        }
+    }
+    return arr;
+}
+
+export {getOwnProps};
