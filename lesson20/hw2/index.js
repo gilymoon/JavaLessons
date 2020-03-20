f8b7ed30b02678355fef806d090137f4146d6504
unknown <gilyjmoon@gmail.com>
2020-03-20 21:42:47 +0200

    1

---
+++ lesson20/hw2/index.js
@@ -0,0 +1,42 @@
+class User {
+    constructor(id, name, sessionId){
+        this._id = id;
+        this._name = name;
+        this._sessionId = sessionId;
+    }
+    get id(){
+        return this._id;
+    }
+    get name(){
+        return this.name;
+    }
+    get sessionId(){
+        return this._sessionId;
+    }
+
+}
+
+class UserRepository {
+    constructor(arr){
+        this._users = Object.freeze(arr[User]);
+    }
+
+    get users(){
+        return this._users;
+    }
+
+    getUserNames(){
+        return this._users.map(el => User.name);
+    }
+    getUserIds(){
+        return this._users.map(el => User.id);
+    }
+    getUserNameById(id){
+        return this._users.map(el => {
+            if(el.id === id) return el.name;
+        })
+    }
+
+}
+
+export {User, UserRepository}
