338b8d7c8f653280b28ce53cd705e7aec0327f20
unknown <gilyjmoon@gmail.com>
2020-03-22 15:37:18 +0200

    2

--- lesson21/hw10/index.js
+++ lesson21/hw10/index.js
@@ -1,12 +1,12 @@
 function manageClasses(){
     const one = document.querySelector('.one');
-    one.classList.add('.selected');
+    one.classList.add('selected');
     const two = document.querySelector('.two');
-    two.classList.remove('.selected');
+    two.classList.remove('selected');
     const three = document.querySelector('.three');
-    three.classList.toggle('.three_done');
+    three.classList.toggle('three_done');
     const four = document.querySelector('.four');
-    four.classList.toggle('.another-class');
+    four.classList.toggle('another-class');
 }
 
 export{manageClasses}
