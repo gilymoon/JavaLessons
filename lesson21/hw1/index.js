2bf4539e74432f3427650fa81d6f49eb986203b5
unknown <gilyjmoon@gmail.com>
2020-03-22 11:39:57 +0200

    1

---
+++ lesson21/hw1/index.js
@@ -0,0 +1,13 @@
+function getTitleElement(){
+    const titleElem = document.querySelector('.title');
+    console.dir(titleElem);
+    return titleElem;
+}
+
+function getInputElement(){
+    const inputElem = document.querySelector('input', 'text');
+    console.dir(inputElem);
+    return inputElem;
+}
+
+export {getInputElement, getTitleElement}
