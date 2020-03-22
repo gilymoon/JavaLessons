6dfe06f21cb05d1b3e502a9b247e0758418fef16
unknown <gilyjmoon@gmail.com>
2020-03-22 11:56:30 +0200

    1

---
+++ lesson21/hw3/index.js
@@ -0,0 +1,17 @@
+function getTitle(){
+    return document.querySelector('.title').textContent;
+}
+
+function getDescription(){
+    return document.querySelector('.about').innerText;
+}
+
+function getPlans(){
+    return document.querySelector('.plans').innerHTML;
+}
+
+function getGoal(){
+    return document.querySelector('.goal').outerHTML;
+}
+
+export {getTitle, getPlans, getGoal, getDescription}
