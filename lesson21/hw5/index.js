ad3f4a2ebbdf974252b08dd84dd8d59cbef94be3
unknown <gilyjmoon@gmail.com>
2020-03-22 12:08:28 +0200

    1

--- lesson21/hw5/index.js
+++ lesson21/hw5/index.js
@@ -1,5 +1,6 @@
 function setButton(buttonText){
     document.querySelector('body').innerHTML = '<button>button text</button>';
+    document.querySelector('body').textContent = '<button>button text</button>';
 }
 
 export {setButton}
