2065fd2cfb7fd34aee4a299b30e91a9892703f58
unknown <gilyjmoon@gmail.com>
2020-03-22 12:50:31 +0200

    1

--- lesson21/hw8/index.js
+++ lesson21/hw8/index.js
@@ -1,6 +1,6 @@
 function finishList(){
     const list = document.querySelector('.list');
-    const one = document.createElement('li').textContent = 1;
+    const one = document.createElement('li').textContent = '1';
     list.prepend(one);
     const four = document.createElement('li').textContent = 4;
     document.querySelector('.special').before(four);
