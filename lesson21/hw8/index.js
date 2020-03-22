04e07aaac5da8153029835383798715c5d0a42b1
unknown <gilyjmoon@gmail.com>
2020-03-22 12:53:04 +0200

    1

--- lesson21/hw8/index.js
+++ lesson21/hw8/index.js
@@ -2,9 +2,11 @@ function finishList(){
     const list = document.querySelector('.list');
     const one = document.createElement('li').textContent = '1';
     list.prepend(one);
-    const four = document.createElement('li').textContent = 4;
+    const four = document.createElement('li').textContent = '4';
     document.querySelector('.special').before(four);
-    const eight = document.createElement('li').textContent = 8;
+    const six = document.createElement('li').textContent = '6';
+    document.querySelector('.special').after(six);
+    const eight = document.createElement('li').textContent = '8';
     list.append(eight);
 }
 
