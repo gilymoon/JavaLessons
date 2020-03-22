80f836a78b0f766bb08c9ab4b6c188be7cdb2434
unknown <gilyjmoon@gmail.com>
2020-03-22 12:49:29 +0200

    1

---
+++ lesson21/hw8/index.js
@@ -0,0 +1,11 @@
+function finishList(){
+    const list = document.querySelector('.list');
+    const one = document.createElement('li').textContent = 1;
+    list.prepend(one);
+    const four = document.createElement('li').textContent = 4;
+    document.querySelector('.special').before(four);
+    const eight = document.createElement('li').textContent = 8;
+    list.append(eight);
+}
+
+export {finishList}
