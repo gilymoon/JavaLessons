7787fe2fabba11291aed08eed621def1bdb67b11
unknown <gilyjmoon@gmail.com>
2020-03-23 19:09:46 +0200

    1

--- lesson22/hw6/index.js
+++ lesson22/hw6/index.js
@@ -4,4 +4,4 @@ function handleClick(event){
    console.log(event.target.textContent) ;
 }
 
-allBtns[0].addEventListener('click', handleClick);
+allBtns.addEventListener('click', handleClick(allBtns[0]));
