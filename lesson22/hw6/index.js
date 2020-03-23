8d0c78b4cb8f822f3e6a15465468b8ffd902785f
unknown <gilyjmoon@gmail.com>
2020-03-23 19:04:58 +0200

    2

--- lesson22/hw6/index.js
+++ lesson22/hw6/index.js
@@ -4,5 +4,4 @@ function handleClick(event){
    console.log(event.target.textContent) ;
 }
 
-btn.addEventListener('click', handleClick(allBtns[0]));
-btn.addEventListener('click', handleClick(allBtns[1]));
+btn.addEventListener('click', handleClick);
