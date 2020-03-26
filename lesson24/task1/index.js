9e753c8fdb34de7aeede04dd500589d96c8565e8
unknown <gilyjmoon@gmail.com>
2020-03-26 15:01:44 +0200

    1

--- lesson24/task1/index.js
+++ lesson24/task1/index.js
@@ -1,5 +1,5 @@
+const weekDays = ['Sun', 'Mon', 'Tue','Wed','Thu','Fri','Str'];
 const dayOfWeek = (date, days) => {
-    const weekDays = ['Sun', 'Mon', 'Tue','Wed','Thu','Fri','Str']
     const day = new Date(date).getDate();
     const dateInFuture = new Date(date).setDate(day + days);
 
