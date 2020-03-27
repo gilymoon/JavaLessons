39424f3acef0126963b30930ec5a62a03317b0cc
unknown <gilyjmoon@gmail.com>
2020-03-27 15:40:45 +0200

    6

--- lesson24/hw2/index.js
+++ lesson24/hw2/index.js
@@ -2,24 +2,13 @@ const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', '
 
 function studentsBirthDays(students){
   
-    const sortedStudents = students.sort((a, b) => {
-        let aStudentBirth = a.birthDate;
-        const aStudentDateArr = aStudentBirth.split('/');
-        const aDateStudent = new Date(aStudentDateArr);
-        let bStudentBirth = b.birthDate;
-        const bStudentDateArr = bStudentBirth.split('/');
-        const bDateStudent = new Date(bStudentDateArr);
-        if(aDateStudent > bDateStudent){
-            return 1;
-        }
-    })
+    const sortedStudents = students.sort((a, b) => new Date(a.birthDate).getDate() - new Date(b.birthDate).getDate());
     const result = sortedStudents.reduce((acc, { name, birthDate }) => {
         const monthsName = months[new Date(birthDate).getMonth()];
         return {...acc, [monthsName]: acc[monthsName] ? acc[monthsName].concat(name) : [name] };
     }, {});
     return result;
 }
-
 
 export{studentsBirthDays}
 
