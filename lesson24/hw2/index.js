d939e110f2a5103edaf70bc18bc474d4beea0ce5
unknown <gilyjmoon@gmail.com>
2020-03-27 13:18:04 +0200

    3

--- lesson24/hw2/index.js
+++ lesson24/hw2/index.js
@@ -15,4 +15,3 @@ function studentsBirthDays(students){
 
 export{studentsBirthDays}
 
-[{name: 'Tom', birthDate:'01/15/2010'}, {name:'Ben', birthDate:'01/17/2008'}, {name: 'Sam', birthDate:'03/15/2010'}]
