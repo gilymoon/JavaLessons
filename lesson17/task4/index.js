232260d62f6f2b9eee605b4fdd311f397167f997
unknown <gilyjmoon@gmail.com>
2020-04-06 10:55:39 +0300

    1

---
+++ lesson17/task4/index.js
@@ -0,0 +1,84 @@
+/* ===> 1 <=== */
+const student = {
+    name: 'Tom',
+};
+
+export function sayName() {
+    console.log(this.name);
+}
+
+// вызовите ф-цию sayName так, чтобы в консоль вывелось имя студента 
+// ... your code here
+
+const func1 = student;
+func1.sayName();
+
+
+// вызовите ф-цию sayName так, чтобы в консоль вывелось имя 'Bruce' (используйте другой объект)
+// ... your code here
+
+const func2 = student;
+func2.call({name: 'Bruce'});
+
+/* ===> 2 <=== */
+
+// вызовите ф-цию greeting так, чтобы в консоль вывелось 
+// 'Hello, Bob Marley. Welcome to the Microsoft'
+// используйте объект company
+// ... your code here
+const company = {
+    companyName: 'Microsoft'
+};
+
+function greeting(firstName, lastName) {
+    console.log(`Hello, ${firstName} ${lastName}. Welcome to the ${this.companyName}`);
+}
+
+const func3 = greeting;
+func3.apply(company, ['Bob', 'Marley'])
+
+
+/* ===> 3 <=== */
+const country = {
+    countryName: 'Ukraine',
+    capital: 'Kyiv'
+};
+
+function getPopulation(population) {
+    return `Population in ${this.countryName} is ${population}`;
+}
+
+// вызовите ф-цию getPopulation так, чтобы она вернула 
+// 'Population in Ukraine is 43000'
+// используйте объект country
+// ... your code here
+
+const func4 = getPopulation;
+func4.call(country, 43000);
+
+
+/* ===> 4 <=== */
+const transaction = {
+    amount: 1200,
+    operation: 'sell',
+    currency: 'USD',
+    exchange: 'NYSE',
+    printTransaction() {
+        console.log(`${this.amount} ${this.currency} - ${this.operation} on ${this.exchange}`);
+    }
+}
+
+const anotherTransaction = {
+    amount: 400,
+    operation: 'buy',
+    currency: 'USD',
+    exchange: 'NASDAQ',
+};
+
+// вызовите метод transaction.printTransaction так, чтобы в консоль вывелось
+// '400 USD - buy on NASDAQ'
+// используйте объект anotherTransaction как контекст
+// ... your code here
+
+const func5 = transaction.printTransaction;
+func5.call(anotherTransaction);
