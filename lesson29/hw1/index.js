f6ee6214e4534c3d26cc004e963193b18ed6d911
unknown <gilyjmoon@gmail.com>
2020-04-06 15:03:48 +0300

    1

--- lesson29/hw1/index.js
+++ lesson29/hw1/index.js
@@ -1,11 +1,11 @@
 function requestUserData(userId, callback){
-    const obj = {name: 'John', 'userId': `${userId}`};
+    const obj = {'userId': `${userId}`, email: `${userId}@example.com`};
     if(userId === 'broken'){
         callback(null, 'Failed to load user data');
     }
     else{
 
-        setTimeout(callback(obj), Math.random()*3);
+        setTimeout(callback(obj), Math.random()*(3-1)+1);
     }
 }
 
