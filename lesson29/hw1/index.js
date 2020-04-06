da5ee3ba2e814c9876245e2f16a4f306c82073aa
unknown <gilyjmoon@gmail.com>
2020-04-06 17:13:00 +0300

    55

--- lesson29/hw1/index.js
+++ lesson29/hw1/index.js
@@ -1,12 +1,19 @@
 function requestUserData(userId, callback){
     const obj = {'userId': `${userId}`, email: `${userId}@example.com`};
+    function getRandomIntInclusive(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
+      }
     if(userId === 'broken'){
         callback(null, 'Failed to load user data');
     }
     else{
 
-        setTimeout(callback(obj), (Math.random()*(3-1)+1)*1000);
+        setTimeout(callback(obj), getRandomIntInclusive(1, 3));
     }
 }
+
+requestUserData('userId777', func);
 
 export {requestUserData}
